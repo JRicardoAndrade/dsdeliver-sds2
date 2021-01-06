10dc889c5288519bc1d9e4aae4545184b8a03bb0
JRicardoAndrade <jricardoandradecosta@gmail.com>
2021-01-05 23:29:10 -0300

    GET /products

---
+++ backend/src/main/java/com/ricardo/dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.ricardo.dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.dsdeliver.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
---
+++ backend/src/main/java/com/ricardo/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.ricardo.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.dsdeliver.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	List<Product> findAllByOrderByNameAsc();
+}
